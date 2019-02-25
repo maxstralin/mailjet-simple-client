c3a36a82977af89d2246cec8abce54ecfad9c88e
Max Strålin <max.stralin@devmasters.se>
2019-02-25 19:49:01 +0100

    Minor test change

--- tests/Mailjet.SimpleClient.Tests/RequestTests.cs
+++ tests/Mailjet.SimpleClient.Tests/RequestTests.cs
@@ -40,6 +40,15 @@ namespace Mailjet.SimpleClient.Tests
             Assert.True(singleMessageInArray.RequestBody["Messages"].Type == JTokenType.Array);
             Assert.True(twoMessages.RequestBody["Messages"].Type == JTokenType.Array);
             Assert.True(singleMessage.RequestBody["Messages"].Type == JTokenType.Array);
+        }
+
+        [Fact]
+        public void Test_ValidateMessageCountInRequest()
+        {
+            var msg = new EmailMessage();
+            var singleMessageInArray = new SendEmailRequest(new[] { msg }, options);
+            var twoMessages = new SendEmailRequest(new[] { msg, msg }, options);
+            var singleMessage = new SendEmailRequest(msg, options);
 
             Assert.True(singleMessageInArray.RequestBody["Messages"].Children().Count() == 1);
             Assert.True(twoMessages.RequestBody["Messages"].Children().Count() == 2);
@@ -68,5 +77,16 @@ namespace Mailjet.SimpleClient.Tests
             //Note that if the value doesn't exit, it returns null. If the value exists, it return a JValue with the value of null!
             Assert.Null(req.RequestBody["Messages"].First[nameof(msg.Id)]);
         }
+
+        [Fact]
+        public void Test_ValidateUserAgent()
+        {
+            var msg = new EmailMessage();
+            var req = new SendEmailRequest(new[] { msg }, options);
+            var message = req.CreateRequest();
+            
+            Assert.True(message.Headers.UserAgent.Single().Product.ToString() == req.UserAgent);
+        }
+
     }
 }
