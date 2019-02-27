9b3baf7ff18bc432248cd0db682e189fcdada1c8
Max Strålin <max.stralin@devmasters.se>
2019-02-27 20:20:18 +0100

    Some comments and a minor namespace move

--- src/Mailjet.SimpleClient.Entities/Models/Emailing/EmailEntity.cs
+++ src/Mailjet.SimpleClient.Entities/Models/Emailing/EmailEntity.cs
@@ -6,8 +6,17 @@ using System.Text.RegularExpressions;
 
 namespace Mailjet.SimpleClient.Entities.Models.Emailing
 {
+    /// <summary>
+    /// An EmailEntity may be a recipient or a sender
+    /// </summary>
+    /// <remarks>I.e. anything that has a name and an email</remarks>
     public class EmailEntity : IEmailEntity
     {
+        /// <summary>
+        /// Create a new instance
+        /// </summary>
+        /// <param name="name">Name of the entity</param>
+        /// <param name="email">Email of the entity</param>
         public EmailEntity(string name, string email)
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
---
+++ src/Mailjet.SimpleClient.Entities/Models/Errors/SendEmailError.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mailjet.SimpleClient.Entities.Models.Errors
+{
+    public class SendEmailError
+    {
+        /// <summary>
+        /// Mailjet's error ID
+        /// </summary>
+        public string ErrorIdentifier { get; set; }
+        /// <summary>
+        /// Error code, e.g. MJ-004
+        /// </summary>
+        public string ErrorCode { get; set; }
+        /// <summary>
+        /// Status code 
+        /// </summary>
+        public int StatusCode { get; set; }
+        /// <summary>
+        /// Human-readable error message
+        /// </summary>
+        public string ErrorMessage { get; set; }
+        /// <summary>
+        /// The properties the error(s) are related to
+        /// </summary>
+        public IEnumerable<string> ErrorRelatedTo { get; set; } = Enumerable.Empty<string>();
+    }
+}
