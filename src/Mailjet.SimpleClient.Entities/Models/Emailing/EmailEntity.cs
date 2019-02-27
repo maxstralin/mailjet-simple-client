b98739f7b0891d281b7ead8503b031906713956a
Max Strålin <max.stralin@devmasters.se>
2019-02-27 19:56:42 +0100

    Added a minor string parser for EmailEntity, with tests

--- src/Mailjet.SimpleClient.Entities/Models/Emailing/EmailEntity.cs
+++ src/Mailjet.SimpleClient.Entities/Models/Emailing/EmailEntity.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Mailjet.SimpleClient.Entities.Models.Emailing
 {
@@ -15,5 +16,18 @@ namespace Mailjet.SimpleClient.Entities.Models.Emailing
 
         public string Name { get; set; }
         public string Email { get; set; }
+
+        /// <summary>
+        /// Parses a string into an EmailEntity
+        /// </summary>
+        /// <param name="emailString">Email string, e.g. Person Perssonsen <person@perssonsen.com></param>
+        /// <returns>Returns null if string couldn't be parsed</returns>
+        public static EmailEntity Parse(string emailString)
+        {
+            var match = Regex.Match(emailString, @"^(?'Name'.*)\s<(?'Email'.*@.*)>$");
+            if (!match.Success) return null;
+            return new EmailEntity(match.Groups["Name"].Value, match.Groups["Email"].Value);
+        }
+
     }
 }
---
+++ tests/Mailjet.SimpleClient.Tests/EntitiesTests.cs
@@ -0,0 +1,25 @@
+﻿using Mailjet.SimpleClient.Entities.Models.Emailing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Mailjet.SimpleClient.Tests
+{
+    public class EntitiesTests
+    {
+        [Fact]
+        public void Test_ParseStringIntoEmailEntity()
+        {
+            var name = "Person Personsson";
+            var email = "person@personsson.com";
+            var validEntity = EmailEntity.Parse($"{name} <{email}>");
+            var invalidEntity = EmailEntity.Parse("invalid");
+
+            Assert.Null(invalidEntity);
+            Assert.IsType<EmailEntity>(validEntity);
+            Assert.True(validEntity.Email == email);
+            Assert.True(validEntity.Name == name);
+        }
+    }
+}
