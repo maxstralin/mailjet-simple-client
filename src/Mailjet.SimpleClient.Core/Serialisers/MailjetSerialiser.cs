d2529be85c7c90174f7304f93c09e4c793aacd27
Max Strålin <max.stralin@devmasters.se>
2019-03-04 21:28:38 +0100

    Added logging to the most important parts

--- src/Mailjet.SimpleClient.Core/Serialisers/MailjetSerialiser.cs
+++ src/Mailjet.SimpleClient.Core/Serialisers/MailjetSerialiser.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
 
 namespace Mailjet.SimpleClient.Core.Serialisers
 {
@@ -17,4 +19,20 @@ namespace Mailjet.SimpleClient.Core.Serialisers
             return JToken.FromObject(obj, serialiser);
         }
     }
+
+    public static class LogSerialiser
+    {
+        public static string Serialise(object obj)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                Converters = new List<JsonConverter>
+                {
+                    new StringEnumConverter()
+                }
+            };
+            return JsonConvert.SerializeObject(obj, Formatting.Indented, settings);
+        }
+    }
+
 }
